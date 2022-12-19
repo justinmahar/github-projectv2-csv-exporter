a42bc65234316fb2baf840860a1fcb3545a2dbc8
Justin Mahar <3472836+justinmahar@users.noreply.github.com>
2022-12-19 12:23:23 -0600

    Revert "feat: support custom project fields"

--- dist/components/GitHubProjectFieldSettings.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import { DivProps } from 'react-html-props';
-export interface GitHubExporterProjectFieldSettings extends DivProps {
-}
-/**
- * Select which fields are included/excluded from the CSV export
- */
-export declare const GitHubProjectFieldSettings: ({ ...props }: GitHubExporterProjectFieldSettings) => JSX.Element;
